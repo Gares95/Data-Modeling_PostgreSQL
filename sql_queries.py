45b79946598744fb471b758b633b700349f20ed8
Guillermo <guillermo.garcia.espuela@gmail.com>
2020-10-06 12:13:53 +0200

    Completed create-tables statements

--- sql_queries.py
+++ sql_queries.py
@@ -1,26 +1,66 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplay_table"
+user_table_drop = "DROP TABLE IF EXISTS user_table_drop"
+song_table_drop = "DROP TABLE IF EXISTS song_table_drop"
+artist_table_drop = "DROP TABLE IF EXISTS artist_table_drop"
+time_table_drop = "DROP TABLE IF EXISTS time_table_drop"
 
 # CREATE TABLES
 
 songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays(
+songplay_id SERIAL PRIMARY KEY, 
+start_time varchar, 
+user_id int, 
+level varchar, 
+song_id varchar, 
+artist_id varchar, 
+session_id varchar, 
+location text, 
+user_agent text
+);
 """)
 
 user_table_create = ("""
+CREATE TABLE IF NOT EXISTS users(
+user_id int PRIMARY KEY, 
+first_name varchar NOT NULL, 
+last_name varchar NOT NULL, 
+gender varchar, 
+level varchar
+);
 """)
 
 song_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs(
+song_id varchar PRIMARY KEY, 
+title varchar NOT NULL, 
+artist_id varchar, 
+year int, duration float
+);
 """)
 
 artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS artists(
+artist_id varchar PRIMARY KEY, 
+name varchar NOT NULL, 
+location text, 
+latitude float, 
+longitude float
+);
 """)
 
 time_table_create = ("""
+CREATE TABLE IF NOT EXISTS time(
+start_time int PRIMARY KEY NOT NULL, 
+hour int, 
+day int, 
+week int, 
+month int, 
+year int, 
+weekday int
+);
 """)
 
 # INSERT RECORDS
